a64f23e4037fd777bf05e2641171ec8dfd05d7c1
pandoedo <122695573+Pandoedo1105@users.noreply.github.com>
2023-01-20 22:58:43 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// //#1 element.innerHTML
+
+// const judul = document.getElementById("judul");
+// judul.innerHTML = "Pando Edo Fernando";
+// judul.style.color = "red";
+
+// const sectionA = document.querySelector("section#a");
+// sectionA.innerHTML = "<div><p>Paragraf1</p></div>";
+
+// 2#element.style.<propertiesCSS>
+// const judul = document.querySelector("#judul");
+// judul.style.color = "lightblue";
+// judul.style.backgroundColor = "salmon";
+// judul.style.textAlign = "center";
+
+// #element.getAttribute
+// const judul = document.getElementsByTagName("h1")[0];
+// judul.setAttribute("name", "pando");
+
+// const a = document.querySelector("section#a a");
+// // a.setAttribute("id", "link");
+// a.getAttribute("href");
+
+// const p2 = document.querySelector(".p2");
+
+// p2.setAttribute("class", "label");
+
+// element.classList
+// const p2 = document.querySelector(".p2");
+// p2.classList.add("label");
+
+// document.body.style.backgroundColor;
